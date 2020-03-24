c3887c8f319e9ae158394729d087e3ead6608feb
shea12underwood <53160612+shea12underwood@users.noreply.github.com>
2020-03-24 19:59:37 -0400

    Add files via upload

--- Prices.py
+++ Prices.py
@@ -4,7 +4,7 @@ import pandas_datareader as dr
 from datetime import datetime,date,timedelta
 startTime = datetime.now()
 
-# Sells.getSells() #uncomment to refresh the csv file
+Sells.getSells() #uncomment to refresh the csv file
 df = pd.read_csv('Insider.csv',index_col = 0)
 
 infoDict = {}
@@ -16,7 +16,9 @@ def getinfo(ticker,n):
         currentprice = tickerdf.iloc[-1]['Close']
         MA = pd.Series(tickerdf['Close'].rolling(n, min_periods=0).mean(), name='MA')
         currentma = MA[-1]
+        print(f"data gathered for {ticker}")
         return (currentprice,currentma)
+        
     except:
         return ('na','na')
     
@@ -36,9 +38,9 @@ def getMovingAverage(row):
     return infoDict[ticker]["ma"]
 
 df['currentprice'] = df.apply (lambda row: getPrice(row), axis=1)
-df['movingaverage'] = df.apply (lambda row: getMovingAverage(row), axis=1)
+print("Prices gathered")
+df['movingaverage'] = df.apply (lambda row: getMovingAverage(row), axis=1) 
+print("movingaverages gathered")
 df.to_csv('InsiderPrices.csv')
 
 print(f'Execution Time: {datetime.now() - startTime}')
-
-    
--- Sells.py
+++ Sells.py
@@ -1,15 +1,32 @@
 import pandas as pd
 from datetime import datetime
+#Needed to use pandas read html for some reason
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 def getSells():
     startTime = datetime.now()
-    df = pd.read_html("https://www.insidearbitrage.com/insider-sales/?desk=yes")
-    df = df[2]
-    columns = df.iloc[0]
-    df.columns = columns
-    df.drop(df.columns[0],axis=1,inplace=True) #axis 1 chooses the columns
-    df = df[1:]
-    df.to_csv('Insider.csv')
+    NumPages = 3
+    finaldf = pd.DataFrame()
+    transactiontypes = ['buying','sales']
+    pagesscraped = 0
+    for t in transactiontypes:
+        for i in range(3):
+            url = f"https://www.insidearbitrage.com/insider-{t}/?desk=yes&pagenum={i+1}"
+            df = pd.read_html(url)
+            df = df[2]
+            columns = df.iloc[0]
+            df.columns = columns
+            df.drop(df.columns[0],axis=1,inplace=True) #axis 1 chooses the columns
+            df = df[1:]
+            if t == 'buying':
+                df['Type'] = "buy"
+            else:
+                df['Type'] = "sell"
+            frames = [df, finaldf]
+            finaldf = pd.concat(frames)
+            pagesscraped+=1
+            print(f'Pages Scraped : {pagesscraped} - Total Elapsed time = {datetime.now() - startTime}')
+
+    finaldf.to_csv('Insider.csv')
     print(f'CSV File Created - Execution Time: {datetime.now() - startTime}')
-
-
