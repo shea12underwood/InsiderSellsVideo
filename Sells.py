611402efc3e9a01d92eded10c0b855c43728b905
shea12underwood <53160612+shea12underwood@users.noreply.github.com>
2020-04-24 19:41:18 -0400

    Added most recent files

--- Sells.py
+++ Sells.py
@@ -4,14 +4,17 @@ from datetime import datetime
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
+
+
 def getSells():
+
     startTime = datetime.now()
     NumPages = 3
     finaldf = pd.DataFrame()
     transactiontypes = ['buying','sales']
     pagesscraped = 0
     for t in transactiontypes:
-        for i in range(3):
+        for i in range(NumPages):
             url = f"https://www.insidearbitrage.com/insider-{t}/?desk=yes&pagenum={i+1}"
             df = pd.read_html(url)
             df = df[2]
@@ -23,10 +26,13 @@ def getSells():
                 df['Type'] = "buy"
             else:
                 df['Type'] = "sell"
-            frames = [df, finaldf]
+            frames = [finaldf,df]
             finaldf = pd.concat(frames)
             pagesscraped+=1
-            print(f'Pages Scraped : {pagesscraped} - Total Elapsed time = {datetime.now() - startTime}')
+            print(f'{pagesscraped} Pages Scraped : Total Elapsed time = {datetime.now() - startTime}')
 
     finaldf.to_csv('Insider.csv')
     print(f'CSV File Created - Execution Time: {datetime.now() - startTime}')
+
+
+
